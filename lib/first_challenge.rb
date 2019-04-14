6201d07125225d0117f062b73329f2b9bfda8cf3
Janice Pena <jipena94@gmail.com>
2019-04-14 23:24:25 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-  contacts["Freddy Mercury"][:favorite_icecream_flavors] = delete.if "strawberry" 
+  contacts["Freddy Mercury"][:favorite_icecream_flavors]
   contacts
   #remember to return your newly altered contacts hash!
   contacts
