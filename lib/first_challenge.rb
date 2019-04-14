d5ec870daac11b80e4fcc8b7da1f20e847874097
Janice Pena <jipena94@gmail.com>
2019-04-14 23:25:10 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-  contacts["Freddy Mercury"][:favorite_icecream_flavors]
+  contacts
   contacts
   #remember to return your newly altered contacts hash!
   contacts
