6fff478856b590b7f0d2240cdcef5f2392de0b88
Janice Pena <jipena94@gmail.com>
2019-04-14 23:26:00 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-  contacts.delete(strawberry)
+  [:contacts].delete(strawberry)
   contacts
   #remember to return your newly altered contacts hash!
   contacts
