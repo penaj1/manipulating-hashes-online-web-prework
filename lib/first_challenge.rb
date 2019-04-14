3bc3adb9341ccfd2cda46f07990c092830cec841
Janice Pena <jipena94@gmail.com>
2019-04-14 23:29:42 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,7 @@ def first_challenge
     }
   }
   #your code here
-  contacts.each do |person, data|
+  contacts.each do |name, data|
        data.each do |key, value|
           if key == :favorite_icecream_flavors
               value.delete_if do |flavor|
